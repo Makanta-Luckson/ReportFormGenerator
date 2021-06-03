a65bdbb9dae15ca6def00f1d6b7b72bfd46162d2
jayOnechild <jacksonmwanaumo02@gmail.com>
2021-06-03 14:00:07 +0200

    initial commit

---
+++ Templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Computer Studies Report Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a>
+        <a href="pupil.php">Pupil</a>
+    </nav>
+</body>
+</html>
